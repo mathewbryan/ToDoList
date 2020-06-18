751a3cf0503c2b93d5c11ea8f377e585ef843850
mathewb <mathew.bryan@radiuspaymentsolutions.com>
2020-06-18 14:45:03 +0100

    added delete function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
 class TodoList extends Component {
     constructor(props){
@@ -9,11 +10,12 @@ class TodoList extends Component {
           };
         
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e) {
         if (this._inputElement.value !== "") {
-          var newItem = {
+          let newItem = {
             text: this._inputElement.value,
             key: Date.now()
           };
@@ -32,6 +34,16 @@ class TodoList extends Component {
         e.preventDefault();
       }
 
+      deleteItem(key) {
+        let filteredItems = this.state.items.filter(function (item) {
+          return (item.key !== key);
+        });
+       
+        this.setState({
+          items: filteredItems
+        });
+      } 
+
     render() {
         return(
         <div className="todoListMain">
@@ -44,7 +56,8 @@ class TodoList extends Component {
                     <button type="submit">add</button>
                 </form>
              </div> 
-             <TodoItems entries={this.state.items}/>
+             <TodoItems entries={this.state.items}
+              delete={this.deleteItem}/>
         </div>  
         )
     }
